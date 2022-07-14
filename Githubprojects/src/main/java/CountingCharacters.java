57e38da752cbaa88b40fbb0a93225e39dd6ea8ee
019970744 <019970744@IBM-PF38PFDJ>
2022-07-14 17:19:08 +0530

    committing

---
+++ Githubprojects/src/main/java/CountingCharacters.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CountingCharacters {
+
+	public static void main(String[] args) {
+		String s = "Ramakrishnan";
+		List<String> list = Arrays.asList(s);
+		for (int i =0;i<-s.length();i++) {
+			System.out.println(i);
+		}
+
+		
+		
+	}
+
+}
